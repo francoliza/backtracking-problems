b66a12164d5e77e13f35126b0dea30e9a3291596
Franco Liza <franco.s.liza@gmail.com>
2021-03-29 22:43:54 -0300

    uploading n-queen problem

---
+++ n-queens/main.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+void printMatrix(std::vector<std::vector<int>> matrix) {
+    for (int i = 0; i < matrix.size(); i++) {
+       for (int j = 0; j < matrix.size(); j++) {
+           std::cout << " " << matrix[i][j];
+        }
+        std::cout << std::endl;
+    }
+}
+
+int isThreatened(std::vector<std::vector<int>> &matrix, int izq, int column) { // pregunta si la reina está amenazada
+   if(izq == 0 && column == 0) {
+       return 0;
+   } else {
+        int diagonal;
+        for (int i = column - 1; i > -1; i--) {
+            if (matrix[izq][i] == 1) {
+                return 1;
+            }
+        }
+
+       diagonal = izq + 1;
+        //Up diagonal
+        for (int i = column - 1; i > -1 && diagonal < matrix.size(); i--) {
+            if (matrix[diagonal][i] == 1) {
+                return 1;
+            }
+            diagonal++;
+        }
+        
+        diagonal = izq - 1;
+        //Down diagonal
+        for (int i = column - 1; i > -1 && diagonal > -1; i--) {
+            if (matrix[diagonal][i] == 1) {
+                return 1;
+            }
+            diagonal--;
+        }
+   } 
+   return 0;
+}
+
+int nqueens(std::vector<std::vector<int>> &matrix, int column, int n) {
+    if (column == n) {
+        return 1;
+    }
+
+   for (int i = 0; i < n; i++) {
+        if (!isThreatened(matrix, i, column)) {
+            matrix[i][column] = 1;
+            if (nqueens(matrix, column + 1, n) == 1) {
+                return 1;
+            }
+            matrix[i][column] = 0;
+       }
+   }
+    
+    return 0;
+}
+
+int main(int argc, char** argv) {
+   // terminé
+    int n  = std::stoi(argv[1]);
+    std::vector<std::vector<int>> matrix (n , std::vector<int> (n, 0));
+
+    nqueens(matrix, 0, n);
+    printMatrix(matrix);
+    return 0;
+}
+
